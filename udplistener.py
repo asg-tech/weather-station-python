7f91e867cebaf11423662dcb05c2cb02ceea403d
unknown <gauranga@asgtech.co.nz>
2020-07-23 15:39:27 +1200

    Template again

--- udplistener.py
+++ udplistener.py
@@ -5,4 +5,5 @@ def runthis():
     fedclass = udpclasses.Evt_precip("first val","Thanks Glenn",3,4)
     print(fedclass.serial_number, fedclass.typee, fedclass.evt, fedclass.hub_sn)
     print(fedclass.doEvtPrecip())
+    
 runthis()
