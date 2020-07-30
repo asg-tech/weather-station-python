a43ac48b1ef30915ea6b34775c067b08cd0ac4f9
unknown <gauranga@asgtech.co.nz>
2020-07-30 12:43:58 +1200

    Better working Copy of class fille.

--- udpclasses.py
+++ udpclasses.py
@@ -10,13 +10,15 @@ class Evt_precip:
         self.typee = typee
         self.hub_sn = hub_sn
         self.evt = evt
+        self.unpacker()
+        self.printme()
 
-    def unpacker(self,evt):
-        timeep = evt[0]
-        return timeep
+    def unpacker(self):
+        self.timeep = self.evt[0]
 
-    def printme(self,time_epoch):
-        return "THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {}".format(self.serial_number, self.typee, self.hub_sn, time_epoch)
+    def printme(self):
+        #return "THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {}".format(self.serial_number, self.typee, self.hub_sn, time_epoch)
+        print("THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {}".format(self.serial_number, self.typee, self.hub_sn, self.timeep))
 
 class Evt_strike:
 
@@ -25,15 +27,16 @@ class Evt_strike:
         self.typee = typee
         self.hub_sn = hub_sn
         self.evt = evt
+        self.unpacker()
+        self.printme()
 
-    def unpacker(self,evt):
-        timeep = evt[0]
-        Distance = evt[1]
-        Energy = evt[2]
-        return timeep, Distance, Energy
+    def unpacker(self):
+        self.timeep = self.evt[0]
+        self.Distance = self.evt[1]
+        self.Energy = self.evt[2]
 
-    def printme(self,time_epoch, Distance, Energy):
-        return "THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, Distance: {} meters, Energy: {}".format(self.serial_number, self.typee, self.hub_sn, time_epoch, Distance, Energy)
+    def printme(self):
+        print("THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, Distance: {} meters, Energy: {}".format(self.serial_number, self.typee, self.hub_sn, self.timeep, self.Distance, self.Energy))
 
 class Rapid_wind:
 
@@ -42,15 +45,27 @@ class Rapid_wind:
         self.typee = typee
         self.hub_sn = hub_sn
         self.ob = ob
+        self.unpacker()
+        self.transform_winddir()
+        self.printme()
 
-    def unpacker(self,ob):
-        timeep = ob[0]
-        windspeed = ob[1]
-        winddirn = ob[2]
-        return timeep, windspeed, winddirn
+    def unpacker(self):
+        self.timeep = self.ob[0]
+        self.windspeed = self.ob[1]
+        self.winddirn = self.ob[2]
 
-    def printme(self,time_epoch, windspeed, winddirn):
-        return "THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, WindSpeed: {} m/sec, WindDirection: {} Degrees".format(self.serial_number, self.typee, self.hub_sn, time_epoch, windspeed, winddirn)
+    def printme(self):
+        print("THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, WindSpeed: {} m/sec, WindDirection: {} Degrees".format(self.serial_number, self.typee, self.hub_sn, self.timeep, self.windspeed, self.winddirn))
+
+    def transform_winddir(self):
+        if self.winddirn > 180 or self.winddirn <= 360:
+            self.winddirn = self.winddirn - 180
+        elif self.winddirn < 180 or self.winddirn >= 0:
+            self.winddirn = self.winddirn + 180
+        elif self.winddirn == 0 or self.winddirn == 360:
+            self.winddirn == 180
+        elif self.windDir == 180:
+            self.windDir == 0
 
 class Obs_air:
 
@@ -60,20 +75,21 @@ class Obs_air:
         self.hub_sn = hub_sn
         self.obs = obs
         self.firmware_revision = firmware_revision
+        self.unpacker()
+        self.printme()
 
-    def unpacker(self,obs):
-        timeep = obs[0][0]
-        stationpress = obs[0][1]
-        airtemp = obs[0][2]
-        relhumid = obs[0][3]
-        lgtnstrike = obs[0][4]
-        lgtnstrikedist_avg = obs[0][5]
-        battery = obs[0][6]
-        reportint = obs[0][7]
-        return timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint
-
-    def printme(self,timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint):
-        return "THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, Station Pressure: {} MB, Air Temp: {} C, Relative Humidity {} %, LightningStrikeCount {}, Lightning Strike Avg Distance {} Km, Battery {}, Report Interval {} Mins".format(self.serial_number, self.typee, self.hub_sn, timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint)
+    def unpacker(self):
+        self.timeep = self.obs[0][0]
+        self.stationpress = self.obs[0][1]
+        self.airtemp = self.obs[0][2]
+        self.relhumid = self.obs[0][3]
+        self.lgtnstrike = self.obs[0][4]
+        self.lgtnstrikedist_avg = self.obs[0][5]
+        self.battery = self.obs[0][6]
+        self.reportint = self.obs[0][7]
+        
+    def printme(self):
+        print("THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, Station Pressure: {} MB, Air Temp: {} C, Relative Humidity {} %, LightningStrikeCount {}, Lightning Strike Avg Distance {} Km, Battery {}, Report Interval {} Mins, Firmware Rev: {}".format(self.serial_number, self.typee, self.hub_sn, self.timeep, self.stationpress, self.airtemp, self.relhumid, self.lgtnstrike, self.lgtnstrikedist_avg, self.battery, self.reportint, self.firmware_revision))
 
 class Obs_sky:
 
@@ -81,8 +97,40 @@ class Obs_sky:
         self.serial_number = serial_number
         self.typee = typee
         self.hub_sn = hub_sn
-        self.obs = obsstationpress
+        self.obs = obs
         self.firmware_revision = firmware_revision
+        self.unpacker()
+        self.transform_winddir()
+        self.printme()
+
+    def unpacker(self):
+        self.timeep = self.obs[0][0]
+        self.illum = self.obs[0][1]
+        self.ultravio = self.obs[0][2]
+        self.rainAccum = self.obs[0][3]
+        self.windLull = self.obs[0][4]
+        self.windAvg = self.obs[0][5]
+        self.windGust = self.obs[0][6]
+        self.windDir = self.obs[0][7]
+        self.battery = self.obs[0][8]
+        self.reportint = self.obs[0][9]
+        self.solarRad = self.obs[0][10]
+        self.locrainAccum = self.obs[0][11]
+        self.precipType = self.obs[0][12]
+        self.windSampInt = self.obs[0][13]
+
+    def transform_winddir(self):
+        if self.windDir > 180 or self.windDir < 360:
+            self.windDir = self.windDir - 180
+        elif self.windDir < 180 or self.windDir > 0:
+            self.windDir = self.windDir + 180
+        elif self.windDir == 0 or self.windDir == 360:
+            self.windDir == 180
+        elif self.windDir == 180:
+            self.windDir == 0
+
+    def printme(self):
+        print("THE UNPACKED VALUES ARE: Serial_Number: {}, type: {}, hub_sn: {}, time_epoch: {} seconds, Illumination: {} Lux, UltraViolet: {} Index, Rain Accumulated: {} mm, Wind Lull: {} m/s, Wind Avg: {} m/s, Wind Gust: {} m/s, Wind Direction: {} Degrees, Battery: {} Volts, Report Interval: {} Minutes, Solar Radiation: {} W/m2, Local Day Rain Accumulation: {} mm, Precipitation Type: {}, Wind Sample Interval: {} secs, Firmware Version: {}".format(self.serial_number, self.typee, self.hub_sn, self.timeep, self.illum, self.ultravio, self.rainAccum, self.windLull, self.windAvg, self.windGust, self.windDir, self.battery, self.reportint, self.solarRad, self.locrainAccum, self.precipType, self.windSampInt, self.firmware_revision))
 
 class Device_status:
 
