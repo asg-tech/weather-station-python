1501b9e54b1291d05c5e59bcf9a33a52a81be2b7
unknown <gauranga@asgtech.co.nz>
2020-07-30 12:45:23 +1200

    Working copy needs two more classes

--- udplistener.py
+++ udplistener.py
@@ -1,11 +1,13 @@
 #This file is a translation to Python of server.ts
+#Wind Direction has been flipped by 180Degrees because the weatherflow tempest unit was designed in the Northern
+#Hemisphere and we are using this device in the Southern Hemisphere.
 import udpclasses
 import json
 import argparse
 
 PATH_JSON = "C:\\Gaurang\\WeatherStation\\"
 
-with open(PATH_JSON + 'air.json') as json_data:
+with open(PATH_JSON + 'sky.json') as json_data:
     json_data = json.load(json_data)
 
 
@@ -16,30 +18,26 @@ def sorter(json_in):
         typee = json_in['type']
         hubsn = json_in['hub_sn']
         event = json_in['evt']
-        precipclass = udpclasses.Evt_precip(serialnum,typee, hubsn,event)
-        timeep = precipclass.unpacker(event)
-        print(precipclass.printme(timeep))
+        udpclasses.Evt_precip(serialnum,typee, hubsn,event)
         #Check with Glen
         # currentData.rain = rain;
         # rained++
-        
+ 
     if (event_type == "evt_strike"):
         serialnum = json_in['serial_number']
         typee = json_in['type']
         hubsn = json_in['hub_sn']
         event = json_in['evt']
-        strikeclass = udpclasses.Evt_strike(serialnum,typee,hubsn,event)
-        timeep, distance, energy = strikeclass.unpacker(event)
-        print(strikeclass.printme(timeep, distance, energy))
+        udpclasses.Evt_strike(serialnum,typee,hubsn,event)
 
     if (event_type == "rapid_wind"):
         serialnum = json_in['serial_number']
         typee = json_in['type']
         hubsn = json_in['hub_sn']
         ob = json_in['ob']
-        windclass = udpclasses.Rapid_wind(serialnum,typee,hubsn,ob)
-        timeep, wspeed, wdirn = windclass.unpacker(ob)
-        print(windclass.printme(timeep, wspeed, wdirn))
+        udpclasses.Rapid_wind(serialnum,typee,hubsn,ob)
+        # timeep, wspeed, wdirn = windclass.unpacker(ob)
+        # print(windclass.printme(timeep, wspeed, wdirn))
 
     if (event_type == "obs_air"):
         serialnum = json_in['serial_number']
@@ -47,9 +45,18 @@ def sorter(json_in):
         hubsn = json_in['hub_sn']
         obs = json_in['obs']
         firmwarerev = json_in['firmware_revision']
-        airclass = udpclasses.Obs_air(serialnum,typee,hubsn,obs,firmwarerev)
-        timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint = airclass.unpacker(obs)
-        print(airclass.printme(timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint))
+        udpclasses.Obs_air(serialnum,typee,hubsn,obs,firmwarerev)
+        # timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint = airclass.unpacker(obs)
+        # print(airclass.printme(timeep, stationpress, airtemp, relhumid, lgtnstrike, lgtnstrikedist_avg, battery, reportint))
+
+    if (event_type == "obs_sky"):
+        serialnum = json_in['serial_number']
+        typee = json_in['type']
+        hubsn = json_in['hub_sn']
+        obs = json_in['obs']
+        firmwarerev = json_in['firmware_revision']
+        udpclasses.Obs_sky(serialnum,typee,hubsn,obs, firmwarerev)
+        
 
 sorter(json_data)
 """     msg_type = json_in.type
